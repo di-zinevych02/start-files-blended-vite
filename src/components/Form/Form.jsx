c9b51b0cf588904e9d0dd8abfc8e5111ffbcba09
Diana Zinevych <dianazinevych02@gmail.com>
2025-03-09 18:51:50 +0100

    should error in Todos

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,67 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
 
-const Form = () => {
-  return <h2>Form</h2>;
+// const Form = ({ onSubmit }) => {
+//   const handleSubmit = (ev) => {
+//     ev.preventDefault();
+//     const value = ev.target.elements.search.value;
+//     if (!value) {
+//       alert('empty')
+//       return;
+//     }
+//     onSubmit(value);
+//     ev.target.reset();
+
+
+//   };
+//   return (
+//   <form className={style.form}>
+//     <button className={style.button} type="submit">
+//       <FiSearch size="16px" />
+//     </button>
+
+//     <input
+//       className={style.input}
+//       placeholder="What do you want to write?"
+//       name="search"
+//       required
+//       autoFocus
+//     />
+//   </form>
+//   );
+// };
+
+// export default Form;
+const Form = ({ onSubmit }) => {
+  const[value, setValue] = useState('');
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    onSubmit(value);
+    if (!value.trim()) {
+      alert("Empty value")
+    }
+    
+
+
+  };
+  const handleChange = (ev) => {
+    setValue(ev.target.value);
+  }
+  return (
+  <form className={style.form}>
+    <button className={style.button} type="submit">
+      <FiSearch size="16px" />
+    </button>
+
+    <input
+      className={style.input}
+      placeholder="What do you want to write?"
+        name="search"
+        value={value}
+        onChange={handleChange}
+      autoFocus
+    />
+  </form>
+  );
 };
 
 export default Form;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,11 +1,25 @@
 import Text from '../components/Text/Text';
+import Form from "../components/Form/Form";
+import { nanoid } from 'nanoid';
 
 const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const addTodo = text => {
+    console.log("text", text);
+    const addTodo = text => {
+      const newTodo = {
+        id: nanoid(5),
+        text: text,
+      };
+      setTodos([...todos, newTodo]);
+  };
+
   return (
     <>
+      <Form onSubmit={addTodo}/>
       <Text textAlign="center">There are no any todos ...</Text>
     </>
   );
 };
 
-export default Todos;
+  export default Todos;
